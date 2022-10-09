f53391b9fc1a6e31d8bf348e6cff556c167b6b54
Olamide Atitebi <olamideumarq@gmail.com>
2022-10-09 08:59:53 +0100

    feat: greetings for multiple names

--- greetings.go
+++ greetings.go
@@ -17,6 +17,24 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named
+// peopele with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	// loop through the received slie of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// in the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // init sets initial values for variables used
 // in the rand package
 
